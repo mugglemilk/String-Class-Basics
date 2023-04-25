eaa78745525955c31d281a37fd5ce90370e299cc
PUMIWATR_K <120041384+mugglemilk@users.noreply.github.com>
2023-04-25 20:05:05 +0700

    Add files via upload

---
+++ CharCode_2.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class CharCode_2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        char c = str.charAt(0);
+        System.out.println((int) c);
+    }
+}
